4f4d3f201136bdce6036bed0551bfc5d3af150cf
zmtzawqlp <zmtzawqlp@live.com>
2020-09-11 15:22:07 +0800

    add github_page branch

--- lib/extended_list.dart
+++
@@ -1,8 +0,0 @@
-library extended_list;
-
-///
-///  create by zmtzawqlp on 2019/11/23
-///
-export 'package:extended_list_library/extended_list_library.dart';
-export 'src/widgets/scroll_view.dart';
-export 'src/widgets/sliver.dart';
